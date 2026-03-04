cb9db036a8d41d51fb18f6e5194bb3f5e573ff9b
LaCumbiaDelCoronavirus <90893484+LaCumbiaDelCoronavirus@users.noreply.github.com>
2026-03-04 17:24:35 +0800

    .

--- Content.Server/_Stalker_DeathmatchArena/ScorestreakReward/ScorestreakRewardSystem.cs
+++ Content.Server/_Stalker_DeathmatchArena/ScorestreakReward/ScorestreakRewardSystem.cs
@@ -2,6 +2,7 @@ using Content.Server.Chat.Managers;
 using Content.Server.Popups;
 using Content.Shared._Stalker_DeathmatchArena.Scorestreak;
 using Content.Shared.Administration.Systems;
+using Content.Shared.Mobs;
 using Content.Shared.Popups;
 using Robust.Shared.Player;
 
@@ -13,13 +14,17 @@ public sealed class ScorestreakRewardSystem : EntitySystem
     [Dependency] private readonly RejuvenateSystem _rejuvenateSystem = default!;
     [Dependency] private readonly PopupSystem _popupSystem = default!;
     [Dependency] private readonly ActorSystem _actorSystem = default!;
+    [Dependency] private readonly ScorestreakSystem _scorestreakSystem = default!;
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<ScorestreakRewardComponent, ScorestreakChangedEvent>(OnScore);
+        SubscribeLocalEvent<ScorestreakRewardComponent, MobStateChangedEvent>(OnStateChanged);
     }
+
+    private string GetName(EntityUid uid) => _actorSystem.GetSession(uid)?.Name ?? $"{Name(uid)} [non-player]";
 
     private void OnScore(Entity<ScorestreakRewardComponent> entity, ref ScorestreakChangedEvent args)
     {
@@ -30,9 +35,23 @@ public sealed class ScorestreakRewardSystem : EntitySystem
         _popupSystem.PopupEntity("You are rejuvenated!", entity, entity, PopupType.Medium);
 
         if (args.NewScore >= entity.Comp.MinScore)
-        {
-            var actName = _actorSystem.GetSession(entity.Owner)?.Name ?? $"{Name(entity.Owner)} [non-player]";
-            _chatManager.DispatchServerAnnouncement($"{actName} has a scorestreak of {args.NewScore}!");
-        }
+            _chatManager.DispatchServerAnnouncement($"{GetName(entity.Owner)} has a scorestreak of {args.NewScore}!");
+    }
+
+    private void OnStateChanged(Entity<ScorestreakRewardComponent> targetEntity, ref MobStateChangedEvent args)
+    {
+        if (args.NewMobState != MobState.Dead ||
+            args.Origin is not { } originUid)
+            return;
+
+        var score = _scorestreakSystem.GetScorestreak(targetEntity.Owner);
+        _scorestreakSystem.SetScore(targetEntity.Owner, 0);
+        if (score < targetEntity.Comp.MinScore)
+            return;
+
+        var originName = GetName(originUid);
+        var targetName = GetName(targetEntity.Owner);
+
+        _chatManager.DispatchServerAnnouncement($"{originName} just ended {targetName}s scorestreak of {score}!");
     }
 }
